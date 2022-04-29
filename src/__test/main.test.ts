cac121305129abb2f9d98a543d73fde9597e8664
rniochy <lniochy@gmail.com>
2022-04-29 14:36:27 +0100

    Resolve the issue with fs module

--- src/__test/main.test.ts
+++ src/__test/main.test.ts
@@ -1,22 +1,42 @@
-class CalendarApp { 
-     
-      constructor (private date_: string ){
-             
-      }
+     import * as fs from 'fs'
+     import path from 'path'
+     class CalendarApp { 
+            constructor (private date_: string ){}
 
-      set date (date: string){
-            this.date_ = date;
-      }
-
-      get date () : string {
-            return this.date_;
+            get date () : string {
+                  return this.date_;
+            }       
       } 
-       
-} 
 
-describe('Should be return event data', ()=> {
-     it('should be create an event', ()=> {
+      interface ICreateEvent {
+            createEvent() : boolean
+            readEvents() : void  
+      }
+      
+      class EventManegment implements ICreateEvent {
+            async addEvent  () : Promise<Boolean> {
+                await fs.writeFile('event.json', 'eventcontent', (err: Error | null)=>{
+                        if(err) return false
+                  }    )
+                  return true;  
+                  }
+            readEvents() : void {
+                   
+            }  
+            createEvent() : boolean {
+                  return true
+            }
+      }
+
+describe('Should be return event data in Calendar App', ()=> {
+     it('should be retur the data event', ()=> {
           const cDate = new CalendarApp('28042022');
            expect(cDate.date).toBe('28042022')
+     })
+})
+describe('Should be create an event in EventManegment', ()=> {
+     it('should be create an event', async ()=> {
+          const createEvent = new EventManegment()
+           expect( await createEvent.addEvent()).toBe(true)
      })
 })
