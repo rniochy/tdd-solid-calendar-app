a0e8802782c9fb480e80e735a3ec506a6b25304f
rniochy <lniochy@gmail.com>
2022-05-06 05:20:18 +0100

    To create IDataFormat interface and DataFormat to format data like id and date to avoid use external library

--- src/__test/main.test.ts
+++ src/__test/main.test.ts
@@ -1,8 +1,10 @@
-     import  {readFile, readFileSync, writeFile, writeFileSync, existsSync} from 'fs'
-     import {join, resolve } from 'path'
+import  {readFile, readFileSync, writeFile, writeFileSync, existsSync} from 'fs'
+import {join, resolve } from 'path'
 
-     class CalendarApp { 
-            constructor (private date_: Date, private description_: string){}
+const PATH_TO_FILE_JSON = join(resolve(), 'src', '__test/events/', 'other.json')
+
+class CalendarApp { 
+      constructor (private date_: Date, private description_: string){}
 
             get date () : Date {
                   return this.date_;
@@ -13,9 +15,10 @@
       } 
 
       interface IEventManegment  {
-            addEvent(eventData: TEventDataFormat):void
-            createEvent() : boolean
-            readEvents() : string
+            addEvent( {date, description}: TEventDataFormat):void
+            deleteEvent(id: Date) : void
+            readEvents() : TEventDataFormat[]
+            editEvent(pros:TEventDataFormat): void
       }
 
       type TEventDataFormat =  {
@@ -23,34 +26,64 @@
             date: Date,
             description:string 
       }
-      const PATH_TO_FILE_JSON = join(resolve(), 'src', '__test/events/', 'other.json')
+
+      interface IDataFormat {
+             idFormat: string
+             date: string // day month year -  
+      }
+      class DataFormat {
+             
+      }
+      
       class EventManegment implements IEventManegment  {
-            addEvent({id, date,description }: TEventDataFormat) : void{    
-                   writeFileSync(PATH_TO_FILE_JSON, JSON.stringify([{id, date, description}]))  
-             }
-            readEvents(): string{        
-                  const name=  readFileSync(PATH_TO_FILE_JSON,{encoding:'utf-8'}) 
-                  return name       
-            }  
-            createEvent() : boolean {
-                  return true
+            editEvent({date, description,id}:TEventDataFormat): void {
+                  const eventsList: Array<TEventDataFormat> = this.readEvents()
+
+                 
             }
+              
+            deleteEvent(id: Date): void {             
+                  const eventsList: Array<TEventDataFormat> = this.readEvents()
+                  eventsList.push({id: "4", date: new Date(), description: "some TTevent"})
+                  const newDataForEventsList = eventsList.filter(events =>  +events.id !==  5); 
+                  writeFileSync(PATH_TO_FILE_JSON, JSON.stringify(newDataForEventsList ))
+            }
+
+            addEvent({date,description }: TEventDataFormat) : void{ 
+                  const array_with_events = this.readEvents()
+                  if(Array.isArray(array_with_events) && array_with_events.length > 0){
+                        array_with_events.push({date, description})
+                        writeFileSync(PATH_TO_FILE_JSON, JSON.stringify(array_with_events))  
+                         return
+                  }
+                  writeFileSync(PATH_TO_FILE_JSON, JSON.stringify([{date, description}]))  
+             }
+            readEvents(): TEventDataFormat[] {        
+                  const value = readFileSync(PATH_TO_FILE_JSON,{encoding:'utf-8'}) 
+                  if(value) return JSON.parse(value) 
+                  return []     
+            }  
       }
 
 describe('Should be return event data in Calendar App', ()=> {
      it('should be retur the data event', ()=> {
-          const cDate = new CalendarApp( new Date, "my event to go party");
-           expect(cDate.date).toBe('28042022')
+            const dateToTest = new Date()     
+            const cDate = new CalendarApp( dateToTest, "my event to go party");
+            expect(cDate.date).toBe(dateToTest)
      })
 })
 describe('Should be managment an event in EventManegment', ()=> {
-     it('should be create an event', async ()=> {
+     it('should be create an event',()=> {
           const createEvent = new EventManegment()
-           expect(await createEvent.addEvent({id: '2', date: new Date(), description:'Time to at party'})).toBeUndefined()
+           expect(createEvent.addEvent({date: new Date(), description:' to at party'})).toBeUndefined()
      })
+
      it('should be read all event', ()=> {
+            const createEvent = new EventManegment()
+            expect(createEvent.readEvents()).toBeTruthy()
+     })
+     it('should be delete a event', ()=> {
           const createEvent = new EventManegment()
-           const value = JSON.stringify([{id: '2', date: new Date(), description:'Time to at party'}])
-           expect(createEvent.readEvents()).toEqual(value)
+           expect(createEvent.deleteEvent(new Date)).toBeUndefined();
      })
 })
